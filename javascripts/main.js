c96b15e1d23f3a1bc1fd1e2605810649628b5656
Javier Rebagliatti <jrebagliatti>
2016-09-12 16:23:33 -0300

    [WIP] Added basic lateral menu using the categories obtained from posts' metadata

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,5 +5,11 @@
         if (window.interactions) {
             window.interactions.setup();
         }
+
+        $(".search-filter-button").on("click", function () {
+            var navTrigger = $("#nav-trigger");
+            navTrigger.prop("checked", !navTrigger.prop("checked"));
+            return false;
+        });
     });
 }(jQuery));
